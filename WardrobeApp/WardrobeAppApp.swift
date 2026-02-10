7e021a1006b3e4f0da4ebc67a36bfd0d5e9daa67
iammgroot <shinminseo2004@gmail.com>
2026-02-10 21:42:49 +0900

    Initial Commit

---
+++ WardrobeApp/WardrobeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeAppApp.swift
+//  WardrobeApp
+//
+//  Created by 신민서 on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
